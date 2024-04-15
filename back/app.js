0a3c1a3a23d32c3bdd27e6e050fbe7ca9469daaf
Levi <levisn@gmail.com>
2024-04-15 12:30:54 -0300

    new features

--- back/app.js
+++ back/app.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 const port = 8000
+const db = require('./db')
 
 var corsOptions = {
     origin: process.env.HOST_ORIGIN,
@@ -36,12 +37,46 @@ app.get('/events', (req, res) => {
     })
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log(req.body)
-    req.body.data['id'] = '3';
+
+    let result
+    try {
+        req.body.data.attributes['started-at'] = new Date()
+        result = await db.query(`INSERT INTO public.events (name, qty_people, location, started_at) VALUES ($1,$2,$3,$4) RETURNING id`,[
+            req.body.data.attributes.name,
+            req.body.data.attributes['qty-people'],
+            req.body.data.attributes.location,
+            req.body.data.attributes['started-at']
+        ])
+    }catch(e){
+        res.status(500).json({
+            error: true,
+            msg: e.message
+        })
+    }
+
+    req.body.data['id'] = result.rows[0].id;
     res.send(req.body)
 })
 
 app.listen(port, () => {
+    db.query(`CREATE TABLE IF NOT EXISTS public.users (
+        id serial4 NOT NULL,
+        "name" varchar(150) NOT NULL,
+        email varchar(250) NOT NULL,
+        pass varchar(150) NULL,
+        created_at timestamp NOT NULL,
+        uipdated_at timestamp NULL,
+        CONSTRAINT users_pk PRIMARY KEY (id)
+    );`);
+    db.query(`CREATE TABLE IF NOT EXISTS public.events (
+        id serial4 NOT NULL,
+        "name" varchar(150) NOT NULL,
+        qty_people varchar(250) NOT NULL,
+        location varchar(150) null,
+        started_at timestamp NOT NULL,
+        CONSTRAINT events_pk PRIMARY KEY (id)
+    );`);
     console.log(`Example app listening on port ${port}`)
 })
